aa5d96d1e84e3f46ed6ebb9b5fc9b0912b287d55
Ajvlogger <ajvlogger4@gmail.com>
2026-02-04 12:52:33 +0800

    Update blockchain.cpp

--- src/blockchain.cpp
+++ src/blockchain.cpp
@@ -1,93 +1,16 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <ctime>
-#include <sstream>
-#include <iomanip>
-#include <openssl/sha.h>
+#include "block.h"
 
 using namespace std;
 
-/* ---------- DOUBLE SHA256 ---------- */
-string doubleSHA256(const string& input) {
-    unsigned char hash1[SHA256_DIGEST_LENGTH];
-    unsigned char hash2[SHA256_DIGEST_LENGTH];
-
-    SHA256((unsigned char*)input.c_str(), input.size(), hash1);
-    SHA256(hash1, SHA256_DIGEST_LENGTH, hash2);
-
-    stringstream ss;
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-        ss << hex << setw(2) << setfill('0') << (int)hash2[i];
-    }
-    return ss.str();
-}
-
-/* ---------- MEDOR (BITCOIN nBits STYLE) ---------- */
-string medorToTarget(uint32_t medor) {
-    uint32_t exponent = medor >> 24;
-    uint32_t mantissa = medor & 0xFFFFFF;
-
-    stringstream ss;
-    ss << hex << mantissa;
-    string target = ss.str();
-
-    int zeros = (exponent - 3) * 2;
-    return string(zeros, '0') + target;
-}
-
-/* ---------- BLOCK HEADER ---------- */
-class Block {
-public:
-    int version;
-    string previousHash;
-    string merkleRoot;
-    uint32_t timestamp;
-    uint32_t medor;   // renamed from bits
-    uint32_t nonce;
-    string hash;
-
-    Block(string prevHash, string data, uint32_t medorVal) {
-        version = 1;
-        previousHash = prevHash;
-        merkleRoot = doubleSHA256(data);
-        timestamp = time(nullptr);
-        medor = medorVal;
-        nonce = 0;
-        hash = mine();
-    }
-
-    string headerToString() {
-        return to_string(version) +
-               previousHash +
-               merkleRoot +
-               to_string(timestamp) +
-               to_string(medor) +
-               to_string(nonce);
-    }
-
-    string mine() {
-        string target = medorToTarget(medor);
-
-        while (true) {
-            string h = doubleSHA256(headerToString());
-            if (h.substr(0, target.size()) <= target) {
-                cout << "Block mined: " << h << endl;
-                return h;
-            }
-            nonce++;
-        }
-    }
-};
-
-/* ---------- BLOCKCHAIN ---------- */
 class Blockchain {
 public:
     vector<Block> chain;
     uint32_t medor;
 
     Blockchain() {
-        medor = 0x1e00ffff; // Bitcoin-equivalent difficulty, MedorCoin name
+        medor = 0x1e00ffff; // Bitcoin-equivalent difficulty
         chain.push_back(createGenesisBlock());
     }
 
