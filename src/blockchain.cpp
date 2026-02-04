20b63b61104d10ddee36f6e1dd29a3b913eae1c8
Ajvlogger <ajvlogger4@gmail.com>
2026-02-05 04:47:04 +0800

    Refactor Blockchain class and reward calculation
    Refactor Blockchain class to improve structure and readability. Consolidate methods and adjust reward calculation.

--- src/blockchain.cpp
+++ src/blockchain.cpp
@@ -1,126 +1,81 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <map>
 #include <ctime>
-#include "block.h"
-#include "crypto.h" // ensure doubleSHA256(string input) exists
+#include "blockchain.h"
+#include "crypto.h"
 
 using namespace std;
 
-class Blockchain {
-public:
-    vector<Block> chain;
-    uint32_t medor;
+Blockchain::Blockchain(string ownerAddr) {
+    ownerAddress = ownerAddr;
+    medor = 0x1e00ffff;
+    totalSupply = 0;
+    maxSupply = 50000000;
 
-    uint64_t totalSupply;
-    uint64_t maxSupply;
+    balances.clear();
 
-    string ownerAddress;
-    map<string, uint64_t> balances; // tracks coins per address
+    // Add genesis
+    chain.push_back(createGenesisBlock());
+}
 
-    // Constructor
-    Blockchain(string ownerAddr) {
-        ownerAddress = ownerAddr;
-        medor = 0x1e00ffff; // difficulty
-        totalSupply = 0;
-        maxSupply = 50000000;
+Block Blockchain::createGenesisBlock() {
+    Block genesis("0", "Genesis Block", medor, ownerAddress);
+    genesis.timestamp = time(nullptr);
 
-        // pre-mine genesis block
-        chain.push_back(createGenesisBlock());
+    genesis.reward = 100;
+    totalSupply += genesis.reward;
+    balances[ownerAddress] += genesis.reward;
+
+    genesis.hash = doubleSHA256(genesis.headerToString());
+    return genesis;
+}
+
+uint64_t Blockchain::calculateReward() {
+    time_t now = time(nullptr);
+    time_t genesisTime = chain[0].timestamp;
+    double months = difftime(now, genesisTime) / (60*60*24*30.0);
+
+    if (months <= 2.0) return 55;
+    return 30;
+}
+
+void Blockchain::mineBlock(Block &block) {
+    string target = "0";
+    do {
+        block.nonce++;
+        block.hash = doubleSHA256(block.headerToString());
+    } while (block.hash.substr(0, target.size()) != target);
+}
+
+void Blockchain::addBlock(const string &data, const string &minerAddr) {
+    uint64_t reward = calculateReward();
+    if (totalSupply + reward > maxSupply)
+        reward = maxSupply - totalSupply;
+
+    Block newBlock(chain.back().hash, data, medor, minerAddr);
+    newBlock.timestamp = time(nullptr);
+    newBlock.reward = reward;
+
+    mineBlock(newBlock);
+    chain.push_back(newBlock);
+
+    uint64_t minerReward = (reward * 90) / 100;
+    uint64_t ownerReward = reward - minerReward;
+
+    balances[minerAddr] += minerReward;
+    balances[ownerAddress] += ownerReward;
+    totalSupply += reward;
+}
+
+void Blockchain::printChain() const {
+    for (size_t i = 0; i < chain.size(); i++) {
+        cout << "Block " << i << ":\n";
+        cout << "  PrevHash: " << chain[i].previousHash << "\n";
+        cout << "  Hash: " << chain[i].hash << "\n";
+        cout << "  Reward: " << chain[i].reward << "\n";
+        cout << "  Miner: " << chain[i].minerAddress << "\n\n";
     }
-
-    // Genesis block
-    Block createGenesisBlock() {
-        Block genesis("0", "Genesis Block", medor, ownerAddress);
-        genesis.timestamp = time(nullptr);
-
-        genesis.reward = 100; // pre-mine 100 MEDOR
-        totalSupply += genesis.reward;
-
-        // Split rewards
-        balances[ownerAddress] += genesis.reward; // all goes to owner in genesis
-
-        genesis.hash = doubleSHA256(genesis.headerToString());
-        return genesis;
-    }
-
-    // Dynamic reward
-    uint64_t calculateReward() {
-        time_t now = time(nullptr);
-        time_t genesisTime = chain[0].timestamp;
-        double months = difftime(now, genesisTime) / (60*60*24*30.0);
-
-        if (months <= 2.0) return 55;
-        else return 30;
-    }
-
-    // Lightweight mining (phone-friendly)
-    void mineBlock(Block &block) {
-        string targetPrefix = "0"; // easy PoW
-        do {
-            block.nonce++;
-            block.hash = doubleSHA256(block.headerToString());
-        } while (block.hash.substr(0, targetPrefix.size()) != targetPrefix);
-    }
-
-    // Add a new block mined by minerAddress
-    void addBlock(const string& data, const string& minerAddr) {
-        uint64_t reward = calculateReward();
-        if (totalSupply + reward > maxSupply)
-            reward = maxSupply - totalSupply;
-
-        Block newBlock(chain.back().hash, data, medor, minerAddr);
-        newBlock.timestamp = time(nullptr);
-        newBlock.reward = reward;
-
-        mineBlock(newBlock);
-
-        chain.push_back(newBlock);
-
-        // Split reward: 90% miner, 10% owner
-        uint64_t minerReward = reward * 0.9;
-        uint64_t ownerReward = reward - minerReward;
-
-        balances[minerAddr] += minerReward;
-        balances[ownerAddress] += ownerReward;
-
-        totalSupply += reward;
-    }
-
-    // Print blockchain and balances
-    void printChain() const {
-        for (size_t i = 0; i < chain.size(); i++) {
-            cout << "Block " << i << ":\n";
-            cout << "  Previous Hash: " << chain[i].previousHash << "\n";
-            cout << "  Data: " << chain[i].data << "\n";
-            cout << "  Hash: " << chain[i].hash << "\n";
-            cout << "  Timestamp: " << chain[i].timestamp << "\n";
-            cout << "  Reward: " << chain[i].reward << " MEDOR\n";
-            cout << "  Miner: " << chain[i].minerAddress << "\n\n";
-        }
-
-        cout << "=== Balances ===\n";
-        for (auto &entry : balances) {
-            cout << entry.first << ": " << entry.second << " MEDOR\n";
-        }
-        cout << "Total Supply: " << totalSupply << " MEDOR\n\n";
-    }
-};
-
-/* ---------- MAIN ---------- */
-int main() {
-    // Owner address
-    string owner = "OWNER_ADDRESS_123";
-
-    Blockchain medorCoin(owner);
-
-    // Miners mine blocks
-    medorCoin.addBlock("First MedorCoin block", "MINER_A");
-    medorCoin.addBlock("Second MedorCoin block", "MINER_B");
-    medorCoin.addBlock("Third MedorCoin block", "MINER_A");
-
-    medorCoin.printChain();
-
-    return 0;
+    cout << "=== Balances ===\n";
+    for (auto &p : balances)
+        cout << p.first << ": " << p.second << " MEDOR\n";
+    cout << "Total: " << totalSupply << " MEDOR\n";
 }
