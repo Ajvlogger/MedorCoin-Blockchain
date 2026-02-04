fcd3a18b96d49202113b80f388d60742b0dec829
Ajvlogger <ajvlogger4@gmail.com>
2026-02-05 05:15:59 +0800

    Refactor Block class to include transactions and update constructors

--- src/block.h
+++ src/block.h
@@ -1,28 +1,50 @@
 #pragma once
 #include <string>
+#include <vector>
+#include <ctime>
+#include "transaction.h"
 
 class Block {
 public:
     std::string previousHash;
-    std::string data;
     std::string hash;
-    uint32_t medor;        // difficulty
+    std::string data;
+
+    uint32_t medor;          // difficulty
+    uint64_t nonce;          // PoW counter
+    uint64_t reward;         // block reward
     time_t timestamp;
-    uint64_t reward;       // reward for this block
-    uint64_t nonce;        // lightweight proof of work
-    std::string minerAddress; // who mined this block
 
-    Block() : medor(0), reward(0), nonce(0), timestamp(0), minerAddress("") {}
+    std::string minerAddress;
 
-    Block(std::string prev, std::string d, uint32_t diff, std::string miner="")
-        : previousHash(prev), data(d), medor(diff),
-          reward(0), nonce(0), timestamp(0), minerAddress(miner) {}
+    std::vector<Transaction> transactions;
+
+    Block() : medor(0), nonce(0), reward(0), timestamp(0) {}
+
+    Block(const std::string& prevHash,
+          const std::string& d,
+          uint32_t diff,
+          const std::string& miner)
+        : previousHash(prevHash),
+          data(d),
+          medor(diff),
+          nonce(0),
+          reward(0),
+          timestamp(time(nullptr)),
+          minerAddress(miner) {}
 
     std::string headerToString() const {
-        return previousHash + data
-            + std::to_string(timestamp)
-            + std::to_string(reward)
-            + std::to_string(nonce)
-            + minerAddress;
+        std::string txData;
+        for (const auto& tx : transactions) {
+            txData += tx.txHash;
+        }
+
+        return previousHash
+             + data
+             + txData
+             + std::to_string(timestamp)
+             + std::to_string(nonce)
+             + std::to_string(reward)
+             + minerAddress;
     }
 };
