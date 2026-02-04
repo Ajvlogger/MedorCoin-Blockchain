66affd1ebd6f1f6eebc6d06eeedc15a0fdba6724
Ajvlogger <ajvlogger4@gmail.com>
2026-02-05 05:38:25 +0800

    Implement key generation in Wallet class
    Refactor wallet key generation into a separate method.

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1,9 +1,25 @@
 #include "wallet.h"
-#include "crypto.h"
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h>
+#include <openssl/rand.h>
+#include <stdexcept>
 
-Wallet::Wallet() {
-    // Generate private key securely
-    privateKey = generatePrivateKey();
-    publicKey = derivePublicKey(privateKey);
-    address = hashPublicKey(publicKey); // SHA256+RIPEMD160
+Wallet::Wallet() {}
+
+void Wallet::generateKeys() {
+    EC_KEY* ecKey = EC_KEY_new_by_curve_name(NID_secp256k1);
+    if (!EC_KEY_generate_key(ecKey)) {
+        throw std::runtime_error("Failed to generate EC key");
+    }
+
+    const BIGNUM* priv = EC_KEY_get0_private_key(ecKey);
+    privateKey.resize(32); // 256-bit key
+    BN_bn2bin(priv, privateKey.data());
+
+    int pubLen = i2o_ECPublicKey(ecKey, nullptr);
+    publicKey.resize(pubLen);
+    unsigned char* pubPtr = publicKey.data();
+    i2o_ECPublicKey(ecKey, &pubPtr);
+
+    EC_KEY_free(ecKey);
 }
