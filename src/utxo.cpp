abe6506c50609827beaebab937e7e1c8f74dc356
Ajvlogger <ajvlogger4@gmail.com>
2026-02-05 12:14:12 +0800

    Update utxo.cpp

--- src/utxo.cpp
+++ src/utxo.cpp
@@ -1,23 +1,33 @@
 #include "utxo.h"
 
-void UTXOSet::addUTXO(const TxOutput& output,
-                      const std::string& txHash,
-                      int index) {
-    std::string key = txHash + ":" + std::to_string(index);
-    utxos[key] = {txHash, index, output.value, output.address};
+// Add a new UTXO to the set
+void UTXOSet::addUTXO(const TxOutput& output, const std::string& txHash, int index) {
+    std::string key = makeKey(txHash, index);
+    utxos[key] = UTXO{txHash, index, output.value, output.address};
 }
 
-void UTXOSet::spendUTXO(const std::string& txHash,
-                        int index) {
-    std::string key = txHash + ":" + std::to_string(index);
-    utxos.erase(key);
+// Remove a spent UTXO from the set
+void UTXOSet::spendUTXO(const std::string& txHash, int index) {
+    std::string key = makeKey(txHash, index);
+    auto it = utxos.find(key);
+    if (it != utxos.end()) {
+        utxos.erase(it);
+    }
 }
 
+// Get total balance for a specific address
 uint64_t UTXOSet::getBalance(const std::string& address) const {
     uint64_t balance = 0;
-    for (const auto& pair : utxos) {
-        if (pair.second.address == address)
-            balance += pair.second.value;
+    for (const auto& [key, utxo] : utxos) {
+        if (utxo.address == address) {
+            balance += utxo.value;
+        }
     }
     return balance;
+}
+
+// Check if a specific UTXO is unspent
+bool UTXOSet::isUnspent(const std::string& txHash, int index) const {
+    std::string key = makeKey(txHash, index);
+    return utxos.find(key) != utxos.end();
 }
