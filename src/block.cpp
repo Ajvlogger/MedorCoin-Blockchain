ca51a93abddafc32022833275be34e39d753382c
Ajvlogger <ajvlogger4@gmail.com>
2026-02-05 11:27:18 +0800

    Refactor Block constructor and headerToString method

--- src/block.cpp
+++ src/block.cpp
@@ -1,20 +1,11 @@
 #include "block.h"
-
 #include <sstream>
-#include <ctime>
 
-// Default constructor
 Block::Block()
-    : previousHash(""),
-      data(""),
-      difficulty(0),
-      minerAddress(""),
-      timestamp(time(nullptr)),
-      nonce(0),
-      reward(0),
-      hash("") {}
+    : previousHash(""), data(""), difficulty(0),
+      minerAddress(""), timestamp(time(nullptr)),
+      nonce(0), reward(0), hash("") {}
 
-// Parameterized constructor
 Block::Block(const std::string& prevHash,
              const std::string& blockData,
              unsigned int diff,
@@ -28,13 +19,8 @@ Block::Block(const std::string& prevHash,
       reward(0),
       hash("") {}
 
-// Convert block header to string for hashing
 std::string Block::headerToString() const {
     std::stringstream ss;
-    ss << previousHash
-       << timestamp
-       << nonce
-       << difficulty
-       << minerAddress;
+    ss << previousHash << timestamp << nonce << difficulty << minerAddress;
     return ss.str();
 }
