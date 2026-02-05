56aaa6e4efcec780ef5cb0fbf787203343235436
Ajvlogger <ajvlogger4@gmail.com>
2026-02-05 15:08:44 +0800

    Update mempool.cpp

--- src/mempool.cpp
+++ src/mempool.cpp
@@ -1,18 +1,25 @@
 #include "mempool.h"
 
-Mempool& Mempool::getInstance() {
-    static Mempool instance;
-    return instance;
+bool Mempool::addTransaction(const Transaction& tx) {
+    std::lock_guard<std::mutex> lock(mtx);
+    if (txMap.find(tx.txHash) != txMap.end()) return false; // Already exists
+    txMap[tx.txHash] = tx;
+    return true;
 }
 
-void Mempool::addTransaction(const Transaction& tx) {
-    transactions.push_back(tx);
+void Mempool::removeTransaction(const std::string& txHash) {
+    std::lock_guard<std::mutex> lock(mtx);
+    txMap.erase(txHash);
 }
 
 std::vector<Transaction> Mempool::getTransactions() {
-    return transactions;
+    std::lock_guard<std::mutex> lock(mtx);
+    std::vector<Transaction> txs;
+    for (auto& kv : txMap) txs.push_back(kv.second);
+    return txs;
 }
 
-void Mempool::clear() {
-    transactions.clear();
+bool Mempool::hasTransaction(const std::string& txHash) {
+    std::lock_guard<std::mutex> lock(mtx);
+    return txMap.find(txHash) != txMap.end();
 }
