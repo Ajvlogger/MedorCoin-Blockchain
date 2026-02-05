8d0c9f20dc25bd6e5912d8a578b514e9ebbfb56f
Ajvlogger <ajvlogger4@gmail.com>
2026-02-05 10:18:01 +0800

    Refactor consensus.h to use pragma once

--- src/consensus.h
+++ src/consensus.h
@@ -1,12 +1,9 @@
-#ifndef CONSENSUS_H
-#define CONSENSUS_H
-
-#include <cstdint> // This ensures that uint32_t and other fixed-width integers are defined
+#pragma once
+#include "block.h"
+#include "blockchain.h"
 
 class Consensus {
 public:
     bool validateBlock(const Block& block, const Block& previousBlock);
     bool validateChain(const Blockchain& chain);
 };
-
-#endif // CONSENSUS_H
