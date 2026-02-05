b05a9e8d11528a95e4bf4c96c8830dceb33f8872
Ajvlogger <ajvlogger4@gmail.com>
2026-02-05 11:33:15 +0800

    Refactor config.h with MedorCoin specific definitions

--- src/config.h
+++ src/config.h
@@ -1,10 +1,21 @@
-#ifndef CONFIG_H
-#define CONFIG_H
+#ifndef MEDORCOIN_CONFIG_H
+#define MEDORCOIN_CONFIG_H
 
-#include <string>
+// MedorCoin configuration values
 
-const std::string COIN_NAME = "MedorCoin";
-const std::string COIN_SYMBOL = "MEDOR";
-const uint32_t INITIAL_MEDOR = 0x1e00ffff; // Difficulty
-
+// Enable or disable debug logging (0 = off, 1 = on)
+#ifndef MEDORCOIN_DEBUG
+#define MEDORCOIN_DEBUG 0
 #endif
+
+// Maximum number of transactions per block
+#ifndef MEDORCOIN_MAX_TRANSACTIONS
+#define MEDORCOIN_MAX_TRANSACTIONS 500
+#endif
+
+// Mining difficulty default (can be modified later)
+#ifndef MEDORCOIN_DEFAULT_DIFFICULTY
+#define MEDORCOIN_DEFAULT_DIFFICULTY 1
+#endif
+
+#endif // MEDORCOIN_CONFIG_H
