e0ea48718402b67c33da42dc18150927baad26f7
Ajvlogger <ajvlogger4@gmail.com>
2026-02-06 06:54:35 +0800

    Update blockdb.cpp

--- src/db/blockdb.cpp
+++ src/db/blockdb.cpp
@@ -1,5 +1,6 @@
 #include "db/blockdb.h"
 
+#include <leveldb/db.h>
 #include <leveldb/write_batch.h>
 #include <iostream>
 
@@ -22,13 +23,15 @@ bool BlockDB::open(const std::string& path) {
     leveldb::Options options;
     options.create_if_missing = true;
 
-    leveldb::Status status = leveldb::DB::Open(options, path, &db);
+    leveldb::DB* tempDb = nullptr;
+    leveldb::Status status = leveldb::DB::Open(options, path, &tempDb);
 
     if (!status.ok()) {
         std::cerr << "BlockDB open failed: " << status.ToString() << std::endl;
         return false;
     }
 
+    db = tempDb;
     return true;
 }
 
@@ -56,6 +59,10 @@ bool BlockDB::writeBlock(const Block& block) {
         key,
         value
     );
+
+    if (!status.ok()) {
+        std::cerr << "BlockDB write failed: " << status.ToString() << std::endl;
+    }
 
     return status.ok();
 }
