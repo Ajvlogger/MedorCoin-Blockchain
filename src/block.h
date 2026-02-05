41475107467fce16e389c359cd9e2f398018e303
Ajvlogger <ajvlogger4@gmail.com>
2026-02-05 10:27:33 +0800

    Refactor Block constructor and headerToString method

--- src/block.h
+++ src/block.h
@@ -9,42 +9,29 @@ public:
     std::string previousHash;
     std::string hash;
     std::string data;
-
-    uint32_t medor;          // difficulty
-    uint64_t nonce;          // PoW counter
-    uint64_t reward;         // block reward
+    uint32_t medor;        // difficulty
+    uint64_t nonce;
+    uint64_t reward;
     time_t timestamp;
-
     std::string minerAddress;
-
     std::vector<Transaction> transactions;
 
-    Block() : medor(0), nonce(0), reward(0), timestamp(0) {}
-
-    Block(const std::string& prevHash,
-          const std::string& d,
-          uint32_t diff,
-          const std::string& miner)
+    Block(const std::string& prevHash = "",
+          const std::string& d = "",
+          uint32_t diff = 0,
+          const std::string& miner = "",
+          uint64_t rw = 0)
         : previousHash(prevHash),
           data(d),
           medor(diff),
           nonce(0),
-          reward(0),
+          reward(rw),
           timestamp(time(nullptr)),
           minerAddress(miner) {}
 
     std::string headerToString() const {
-        std::string txData;
-        for (const auto& tx : transactions) {
-            txData += tx.txHash;
-        }
-
-        return previousHash
-             + data
-             + txData
-             + std::to_string(timestamp)
-             + std::to_string(nonce)
-             + std::to_string(reward)
-             + minerAddress;
+        // Simple string representation for hashing
+        return previousHash + data + std::to_string(medor) +
+               std::to_string(nonce) + minerAddress;
     }
 };
