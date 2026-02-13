5b349de1ac18b2e85a6f92b59ea1b810246d7bb3
Ajvlogger <ajvlogger4@gmail.com>
2026-02-14 02:23:17 +0800

    Update sync_manager.h
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- src/net/sync_manager.h
+++ src/net/sync_manager.h
@@ -1,27 +1,22 @@
 #pragma once
 
 #include <string>
-#include <vector>
 #include "blockchain.h"
-#include "block.h"
 #include <nlohmann/json.hpp>
 
 /**
- * SyncManager - sync blocks between peers
+ * SyncManager handles syncing missing blocks between peers.
  */
 class SyncManager {
 public:
     explicit SyncManager(Blockchain &chain);
 
-    // Called when a peer announces its latest height
+    // Called when a sync_block message arrives
+    void handleSyncBlock(const nlohmann::json &msg);
+
+    // Called when a peer announces its latest chain length
     void handlePeerHeight(const std::string &peerId, uint64_t height);
 
-    // Process a sync block message received from network
-    void handleSyncBlockMsg(const nlohmann::json &msg);
-
-    // Request missing blocks from peer
-    void requestBlocks(const std::string &peerId, uint64_t from);
-
 private:
-    Blockchain &blockchain;
+    Blockchain &chain;
 };
