f4809da3d4a326e94e2fa148985cd7e8786ee08a
Ajvlogger <ajvlogger4@gmail.com>
2026-02-13 19:33:52 +0800

    Update execute.h
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- src/evm/execute.h
+++ src/evm/execute.h
@@ -2,7 +2,7 @@
 #define MEDOR_EVM_EXECUTE_H
 
 #include <vector>
-#include <cstdint>
+#include <string>
 #include <evmc/evmc.hpp>
 
 class EVMStorage;
@@ -10,16 +10,31 @@ class Blockchain;
 
 class EVMExecutor {
 public:
-    static evmc_result executeContract(
+
+    /**
+     * Execute EVM bytecode using EVMC.
+     *
+     * @param storage: access to contract storage
+     * @param bytecode: contract code to run
+     * @param inputData: calldata (msg.data)
+     * @param gasLimit: gas budget for this execution
+     * @param to: destination (contract address)
+     * @param from: sender address
+     * @param chain: reference to blockchain state
+     * @param minerAddress: miner or executor address (for tip reward)
+     *
+     * @return an evmc::Result with status_code, gas_left, and potential return data
+     */
+    static evmc::Result executeContract(
         EVMStorage &storage,
         const std::vector<uint8_t> &bytecode,
         const std::vector<uint8_t> &inputData,
         uint64_t gasLimit,
-        evmc_address to,
-        evmc_address from,
+        const evmc_address &to,
+        const evmc_address &from,
         Blockchain &chain,
         const std::string &minerAddress
     );
 };
 
-#endif
+#endif // MEDOR_EVM_EXECUTE_H
