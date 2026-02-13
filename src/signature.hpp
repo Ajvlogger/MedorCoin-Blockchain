ca5325b95a03db89eb869566182ad77d65a3eddd
Ajvlogger <ajvlogger4@gmail.com>
2026-02-13 10:37:09 +0800

    Delete src/signature.hpp
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- src/signature.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-#include <array>
-
-std::tuple<std::array<uint8_t,32>, std::array<uint8_t,32>, uint8_t>
-signHash(const std::array<uint8_t,32> &digest,
-         const std::string &privKeyPath);
