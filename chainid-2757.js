ff274d6478963db9fff7a872912199aef0fc7055
Ajvlogger <ajvlogger4@gmail.com>
2026-05-23 05:27:14 +0800

    Update Medorcoin Mainnet configuration
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- chainid-2757.js
+++ chainid-2757.js
@@ -1,23 +1,27 @@
 {
-  "name": "Medorcoin Mainnet",
+  "name": {
+    "name": "Medorcoin Mainnet",
+    "history": [
+      {
+        "url": "https://medorcoin.org",
+        "name": "Medorcoin Mainnet"
+      }
+    ]
+  },
   "chain": "MEDOR",
   "rpc": [
-    "http://172.235.50.31:3000"
+    "https://rpc.medorcoin.org"
   ],
   "faucets": [],
   "nativeCurrency": {
     "name": "Medorcoin",
-    "symbol": "MEDOR",
+    "symbol": "MDC",
     "decimals": 18
   },
-  "features": [
-    { "name": "EIP155" }
-  ],
   "infoURL": "https://medorcoin.org",
   "shortName": "medor",
   "chainId": 2757,
   "networkId": 2757,
-  "icon": "medorcoin",
   "explorers": [
     {
       "name": "MedorExplorer",
